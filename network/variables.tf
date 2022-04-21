f47d4d7b84f0f3d498eff92d0ec28a426e0d27cd
kaslie <kaslie@styletheory.co>
2022-04-21 12:53:18 +0700

    Add VPC

--- network/variables.tf
+++ network/variables.tf
@@ -1,15 +1,4 @@
-variable "access_key" {
-  type = string
-}
 
-variable "secret_key" {
-  type = string
-}
-//
-variable "region" {
-  type = string
-  default = "ap-southeast-5"
-}
 
 variable "profile" {
   type = string
