3f07fc3f43edc5c91f09e90e0c46e8c4c787de3f
kaslie <kaslie@styletheory.co>
2022-04-22 15:56:58 +0700

    Combine VPC with NAT

--- network/vpc.tf
+++ network/vpc.tf
@@ -1,19 +1,33 @@
 module "vpc" {
-  source  = "alibaba/vpc/alicloud"
+  source  = "terraform-alicloud-modules/network-with-nat/alicloud"
 
-  create              = true
+  create_vpc          = true
   vpc_name            = "dkatalis"
   vpc_cidr            = "10.10.0.0/16"
 
   availability_zones  = ["ap-southeast-5a", "ap-southeast-5b", "ap-southeast-5c"]
   vswitch_cidrs       = ["10.10.1.0/24", "10.10.20.0/24", "10.10.40.0/24"]
+  vswitch_tags = {
+    Created               = "Terraform"
+    Environment           = "production"
+    type                  = "wordpress-vswitch"
+  }
 
   vpc_tags = {
     terraform   = true
   }
 
-  vswitch_tags    = {
-    environment   = "production"
-    profile       = "dkatalis=test"
+  # NAT
+  create_nat    = true
+  nat_name      = "nat-gateway"
+
+  # EIP
+  create_eip    = true
+  eip_name      = "eip-nat"
+  eip_isp       = "BGP"
+  eip_tags = {
+    Created               = "Terraform"
+    Environment           = "production"
+    type                  = "wordpress-eip"
   }
 }
